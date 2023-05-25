8f5fc7e121181d531a2dd72ac6088a41cb911e65
Fang Yi <yifang@faceunity.com>
2023-05-25 14:02:59 +0800

    fix:ci

--- build.mjs
+++ build.mjs
@@ -30,7 +30,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 if(getVersion()){
     getVersion = function(){
-        return "${createRequire(import.meta.url)('../package.json').version}";
+        return "${createRequire(import.meta.url)('./zx/package.json').version}";
     }
 }
         `
